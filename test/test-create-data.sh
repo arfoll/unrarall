0a0ac20d6670559e53cac4eba401bf62b0fe0640
limelime <limelime@users.noreply.github.com>
2016-12-20 14:25:07 -0500

    Add test data creation cases.

--- test/test-create-data.sh
+++ test/test-create-data.sh
@@ -3,9 +3,36 @@ set -e
 # Description: Create test data.
 
 RAR_DATA_DIR=rar-data
+rm -rf "${RAR_DATA_DIR}"
 mkdir -p "${RAR_DATA_DIR}"
 RAR_DATA_DIR=$(readlink -ev "${RAR_DATA_DIR}")
 
-RAR_FILE=${RAR_DATA_DIR}/data-rar-no-password
-echo "${RAR_FILE}" > "${RAR_FILE}.txt"
-rar a "${RAR_FILE}.rar" "${RAR_FILE}.txt"
+# Case: No password rar.
+(
+  cd "${RAR_DATA_DIR}"
+	RAR_FILE=data-rar-no-password
+	echo "${RAR_FILE}" > "${RAR_FILE}.txt"
+	rar a "${RAR_FILE}.rar" "${RAR_FILE}.txt"
+	rm -f "${RAR_FILE}.txt"
+)
+
+PASSWORD=test
+
+# Case: Encrypted rar.
+(
+  cd "${RAR_DATA_DIR}"
+	RAR_FILE=data-rar-with-password
+	echo "${RAR_FILE}" > "${RAR_FILE}.txt"
+	rar a -p${PASSWORD} "${RAR_FILE}.rar" "${RAR_FILE}.txt"
+	rm -f "${RAR_FILE}.txt"
+)
+
+
+# Case: Header and files encrypted rar.
+(
+  cd "${RAR_DATA_DIR}"
+	RAR_FILE=data-rar-header-files-encrypted
+	echo "${RAR_FILE}" > "${RAR_FILE}.txt"
+	rar a -hp${PASSWORD} "${RAR_FILE}.rar" "${RAR_FILE}.txt"
+	rm -f "${RAR_FILE}.txt"
+)

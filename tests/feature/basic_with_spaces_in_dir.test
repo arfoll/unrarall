arfoll/unrarall

commit d1188a095fbb04a556852d253b9d6d0039202444

Remove a little bit of code duplication

unrarall
@@ -704,22 +704,18 @@ for file in $(${FIND} "$DIR" -depth -iregex '.*\.\(rar\|001\)$'); do
     file_encrypted=$(isRarEncrypted ${UNRARALL_BIN} ${filenameAbsolute})
     if [ "${file_encrypted}" -eq 0 ]; then
       # The file is not encrypted. We're extracting without a password
-      if [ "$VERBOSE" -eq 1 ] || [ "$UNRARALL_BIN" = "echo" ] ; then
-        ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p- "$filenameAbsolute"
-      else
-        ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p- "$filenameAbsolute" >/dev/null
-      fi
+      # Use eval so that redirection gets evaluated correctly
+      eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p- "$filenameAbsolute" \
+        $([ "$VERBOSE" -eq 0 -a "X${UNRARALL_BIN}" != "Xecho" ] && echo ">/dev/null")
       SUCCESS=$?
     else
       # The file is encrypted. if we have a password file, then use it
       if [ -s "${UNRARALL_PASSWORD_FILE}" ] ; then
         [ $VERBOSE -eq 1 ] && message info "This archive is encrypted. Trying passwords from password file \"${UNRARALL_PASSWORD_FILE}\"..."
         while true; do read password || break
-          if [ "$VERBOSE" -eq 1 ] || [ "$UNRARALL_BIN" = "echo" ] ; then
-            ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p"$password" "$filenameAbsolute"
-          else
-            ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p"$password" "$filenameAbsolute" >/dev/null
-          fi
+          # Use eval so that redirection gets evaluated correctly
+          eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p"$password" "$filenameAbsolute" \
+            $([ "$VERBOSE" -eq 0 -a "X${UNRARALL_BIN}" != "Xecho" ] && echo ">/dev/null")
           SUCCESS=$?
           if [ "$SUCCESS" -eq 0 ] ; then
             [ $VERBOSE -eq 1 ] && message ok "Extraction succeeded using password: \"${password}\""

commit 7f3d31eeb7f83366aba6cab9f8b534eb0257d4cd

Fix #38
d1188a095fbb04a556852d253b9d6d0039202444 introduced use of `eval`
which broken the quoted file paths. Try fix by adding extra quotes.

unrarall
@@ -770,7 +770,7 @@ for file in $(find_wrapper "$DIR" -depth -iregex '.*\.(rar|001)$'); do
     if [ "${file_encrypted}" -eq 0 ]; then
       # The file is not encrypted. We're extracting without a password
       # Use eval so that redirection gets evaluated correctly
-      eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p- "$filenameAbsolute" \
+      eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p- "\"${filenameAbsolute}\"" \
         $([ "$VERBOSE" -eq 0 -a "X${UNRARALL_BIN}" != "Xecho" ] && echo ">/dev/null")
       SUCCESS=$?
     else
@@ -779,7 +779,7 @@ for file in $(find_wrapper "$DIR" -depth -iregex '.*\.(rar|001)$'); do
         [ $VERBOSE -eq 1 ] && message info "This archive is encrypted. Trying passwords from password file \"${UNRARALL_PASSWORD_FILE}\"..."
         while true; do read password || break
           # Use eval so that redirection gets evaluated correctly
-          eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p"$password" "$filenameAbsolute" \
+          eval ${UNRARALL_BIN} ${UNRAR_METHOD} $( getUnrarFlags ${UNRARALL_BIN}) -p"\"$password\"" "\"${filenameAbsolute}\"" \
             $([ "$VERBOSE" -eq 0 -a "X${UNRARALL_BIN}" != "Xecho" ] && echo ">/dev/null")
           SUCCESS=$?
           if [ "$SUCCESS" -eq 0 ] ; then

tests/feature/basic_with_spaces_in_dir.test
@@ -1,0 +1,16 @@
+# REQUIRES: rar_binary
+# Remove old temporary working directory
+# RUN: rm -rf "%t SPACES"
+# RUN: mkdir -p "%t SPACES"
+# RUN: cd "%t SPACES"
+
+# Make a simple test file and put it in a simple rar file
+# RUN: echo "Test file" > test_file
+# RUN: %rar a test.rar test_file
+# RUN: test -f test.rar
+# RUN: rm test_file
+
+# Check the test file is extracted and rar file is removed
+# RUN: %unrarall --clean=rar .
+# RUN: test -f test_file
+# RUN: test ! -f test.rar